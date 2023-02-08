76e60cdeed5ef7c41fbb05fb0f6e538e2bcc9683
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-08 15:27:58 +0100

    Fix linting

--- src/Command/ReportingCommand.php
+++ src/Command/ReportingCommand.php
@@ -7,9 +7,7 @@ namespace Dbp\Relay\MonoBundle\Command;
 use Dbp\Relay\MonoBundle\Service\ConfigurationService;
 use Dbp\Relay\MonoBundle\Service\PaymentService;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ReportingCommand extends Command
